5b1b32ae0c85cfe9003053ae89fb0cfc2da23161
Hasna <hasna.elboukhari.neuro@gmail.com>
2023-11-29 02:48:22 -0800

    error fixed

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     for char in str:
         uni_code = ord(char)
-        if uni_code >= 97 and uni_code <= 122:
+        if 97 <= uni_code <= 122:
             uni_code -= 32
             print("{}".format(chr(uni_code)), end='')
-        print()
+            print()
