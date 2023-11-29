8249138009641dd85f193f61825e1c09d3a1f112
Hasna <hasna.elboukhari.neuro@gmail.com>
2023-11-29 06:19:49 -0800

    error fixed

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -69,3 +69,33 @@ void free_listint(listint_t *head)
 		free(current);
 	}
 }
+/**
+ * insert_node - inserts a number into a sorted singly-linked list
+ * @head: a pointer to the head of the linked list
+ * @number: the number to insert
+ * Return: if the function fails - NULL
+ * otherwise - a pointer to the new node
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *node = *head, *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+
+	if (node == NULL || node->n >= number)
+	{
+		new->next = node;
+		*head = new;
+		return (new);
+	}
+	while (node && node->next && node->next->n < number)
+		node = node->next;
+
+	new->next = node->next;
+	node->next = new;
+
+	return (new);
+}
