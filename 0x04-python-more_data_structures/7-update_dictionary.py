9471a6917f1ca2ba637d92906228f4ad8ec71e0e
Hasna <hasna.elboukhari.neuro@gmail.com>
2023-12-07 01:34:58 -0800

    file corrected

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -3,5 +3,7 @@
 def update_dictionary(a_dictionary, key, value):
     """A function that replace or add key/value pairs
     in a dictionary"""
-    a_dictionay[key] = value
-    return a_dictionary
+    if key in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        a_dictionary[key] = value
