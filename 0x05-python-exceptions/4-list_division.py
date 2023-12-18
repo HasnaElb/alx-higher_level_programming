425644e4f191113d3a6ff8ca2b05f4c068f8cfc8
Hasna <hasna.elboukhari.neuro@gmail.com>
2023-12-18 07:24:40 -0800

    files created

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -12,20 +12,14 @@ def list_division(my_list_1, my_list_2, list_length):
         A new list of length list_length containing all the divisions.
     """
 
+    result = []
+
     for i in range(list_length):
         try:
             elem_1 = my_list_1[i] if i < len(my_list_1) else 0
             elem_2 = my_list_2[i] if i < len(my_list_2) else 0
 
-            if elem_2 == 0:
-                raise ZeroDivisionError("division by 0")
-
-            division_result = elem_1 / elem_2
-
-            if not isinstance(division_result, (int, float)):
-                raise TypeError("wrong type")
-
-            result.append(division_result)
+            result.append(elem_1 / elem_2)
 
         except ZeroDivisionError:
             print("division by 0")
@@ -42,4 +36,4 @@ def list_division(my_list_1, my_list_2, list_length):
         finally:
             pass
 
-    return (result)
+    return result
