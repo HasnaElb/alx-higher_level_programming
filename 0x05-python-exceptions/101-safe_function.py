d178c135c6a2d8ad9851f9f55baef70ca44ec6e0
Hasna <hasna.elboukhari.neuro@gmail.com>
2023-12-19 02:07:41 -0800

    error fixed

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -16,6 +16,6 @@ def safe_function(fct, *args):
     try:
         result = fct(*args)
         return result
-    except (ValueError, TypeError) as err:
+    except Exception as err:
         print("Exception: {}".format(err), file=sys.stderr)
         return None
