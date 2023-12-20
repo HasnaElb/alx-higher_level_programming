b125be8880958cdde59fe0e4aa04fabeb34311e7
Hasna <hasna.elboukhari.neuro@gmail.com>
2023-12-20 07:31:21 -0800

    file corrected

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -26,7 +26,7 @@ class Square:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__ = value
+        self.__size = value
 
     @property
     def position(self):
