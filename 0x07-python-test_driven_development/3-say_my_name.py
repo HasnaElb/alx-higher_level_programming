40007689dd59484ba70645943b5f9dc45168b846
Hasna <hasna.elboukhari.neuro@gmail.com>
2024-01-07 09:19:44 -0800

    file created

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""Function that prints my name
-@first_name and last_name must be strings
-Return: only print arg in stdout
+"""Module that prints fullname
 """
 
 
@@ -9,11 +7,8 @@ def say_my_name(first_name, last_name=""):
     """
     function for say name givend in parameters
     """
-
-    if first_name:
-        if type(first_name) is not str:
-            raise TypeError("first_name must be a string")
-        elif type(last_name) is not str:
-            raise TypeError("last_name must be a string")
-        else:
-            print("My name is {} {}".format(first_name, last_name))
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) != str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
