2d5b4ddd420545b5d835d0b4bd92631860e213dd
Hasna <hasna.elboukhari.neuro@gmail.com>
2024-01-09 04:12:50 -0800

    file created

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,6 +4,7 @@
 
 def add_attribute(obj, attribute, value):
     """Attempts to set or update 'attribute' with 'value'.
+    
     Args:
     obj (any): object to have attribute set
     attribute (str): name of new/updated attribute
@@ -13,11 +14,8 @@ def add_attribute(obj, attribute, value):
     TypeError: if adding or updating attribute not possible
     """
     if hasattr(obj, "__dict__"):
-        # if __dict__ is present, attributes can be dynamically added
         setattr(obj, attribute, value)
     elif hasattr(obj, "__slots__") and attribute in ob.__slots__:
-        # even if no __dict__, existing attributes in __slots__ can be updated
         setattr(obj, attribute, value)
     else:
-        # out of options, can't add
         raise TypeError("can't add new attribute")
