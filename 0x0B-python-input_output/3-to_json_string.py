6c226157cef0e4ec56f82011c707ff73bb94f6d9
Hasna <hasna.elboukhari.neuro@gmail.com>
2024-01-10 14:12:36 -0800

    file created

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" create new funtion """
+import json
+
+
+def to_json_string(my_obj):
+    """Convert python to JSON
+
+    Args:
+    my_obj ([dict]): object to convert
+
+    Returns:
+    [str-json]: object convert
+    """
+
+    return json.dumps(my_obj)
