f6d85b83991cb8d0022a840f699056f39f55c7f7
Hasna <hasna.elboukhari.neuro@gmail.com>
2024-01-10 08:22:42 -0800

    file created

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Read file task"""
+
+
+def read_file(filename=""):
+    """Reads contents of a text file and print to stdout.
+
+    Args:
+    filename (str): name of file to be opened
+    """
+    with open(filename, encoding='utf-8') as file:
+        print(file.read(), end='')
