d4ce70a14bb021e2437e8cb2e7a26057d561bd8f
Hasna <hasna.elboukhari.neuro@gmail.com>
2024-01-11 11:41:50 +0000

    file created

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+function for read and safe argv in JSON format in file
+"""
+import sys
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+filename = "add_item.json"
+my_list = []
+try:
+	my_list = load_from_json_file(filename)
+except:
+	pass
+finally:
+	for idx in range(1, len(sys.argv)):
+		my_list.append(sys.argv[idx])
+	save_to_json_file(my_list, filename)
