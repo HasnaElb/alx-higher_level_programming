f5ffe377a0a059a1c6e6bdb702fbd7e427a2e9e3
Hasna <hasna.elboukhari.neuro@gmail.com>
2024-01-12 04:04:29 -0800

    file created

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,36 +1,18 @@
 #!/usr/bin/python3
-"""
-Function to read and save argv in JSON format in a file
-"""
-import sys
-from json import load, dump
+""" task 7"""
 
 
-def load_from_json_file(filename):
-    """Load JSON data from file"""
-    try:
-        with open(filename, 'r') as file:
-            return load(file)
-        except FileNotFoundError:
-            return []
+from sys import argv
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+argv_edit = argv[1:]
 
-def save_to_json_file(data, filename):
-    """Save JSON data to file"""
-    with open(filename, 'w') as file:
-        dump(data, file)
-
-
-def main():
-    """Main function"""
-    json_filename = "add_item.json"
-    my_list = load_from_json_file(json_filename)
-
-    for arg in sys.argv[1:]:
-        my_list.append(arg)
-
-    save_to_json_file(my_list, json_filename)
-
-
-if __name__ == "__main__":
-    main()
+try:
+    content_list = load_from_json_file("add_item.json")
+except:
+    content_list = []
+finally:
+    for arg in argv_edit:
+        content_list.append(arg)
+    save_to_json_file(content_list, "add_item.json")
