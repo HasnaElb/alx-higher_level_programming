c54148e932d0af9e311f28ac71d0b75bbef846c5
Hasna <hasna.elboukhari.neuro@gmail.com>
2024-01-13 01:59:45 -0800

    file created

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""define a new class student"""
+
+
+class Student(object):
+    """class student
+    args:
+    object ([object]): class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """initialize var object
+        Args:
+        first_name ([str]): first name
+        last_name ([str]): last name
+        age ([int]): age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """define a new dict
+        Returns:
+        [dict]: data en format dict
+        """
+        new_dict = {}
+        if attrs is not None:
+            for i in attrs:
+                if i in self.__dict__:
+                    new_dict[i] = self.__dict__[i]
+            return new_dict
+        else:
+            return self.__dict__
