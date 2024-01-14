2985986f7a24f8c5db69a0ce8e65f202d2268cd9
Hasna <hasna.elboukhari.neuro@gmail.com>
2024-01-14 14:51:29 +0000

    file created

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -3,23 +3,23 @@
 
 
 def append_after(filename="", search_string="", new_string=""):
-	"""Insert a line of text to a file, after each line containing
-	a specific string.
+    """Inserts a line of text to a file, after each line containing a specific
+    string.
 
-	Args:
+    Args:
         filename (str): file to search
-    	search_string (str): search term
-	new_string (str): line to insert into file after line containing
-match
-	"""
-	with open(filename, 'r+', encoding='utf-8') as curr_file:
-        	lines = curr_file.readlines()
-        	curr_file.seek(0)
-        	for i, line in enumerate(lines):
-            	# print("read: {}".format(line), end='')
-            		if search_string in line:
-                		lines[i] = line + new_string
-                # print("modded: {}".format(line), end='')
-		# for line in lines:
-		# print(line, end="")
-        	curr_file.writelines(lines)
+        search_string (str): search term
+        new_string (str): line to insert into file after line containing match
+
+    """
+    with open(filename, 'r+', encoding='utf-8') as curr_file:
+        lines = curr_file.readlines()
+        curr_file.seek(0)
+        for i, line in enumerate(lines):
+            #            print("read:   {}".format(line), end='')
+            if search_string in line:
+                lines[i] = line + new_string
+                #                print("modded: {}".format(line), end='')
+#        for line in lines:
+#            print(line, end="")
+        curr_file.writelines(lines)
