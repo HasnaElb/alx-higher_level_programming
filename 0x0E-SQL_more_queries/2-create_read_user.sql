c5f76466c05d1f7d1c00c852bb5e05fadc883d93
Hasna <hasna.elboukhari.neuro@gmail.com>
2024-02-14 12:02:24 +0000

    file created

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
---creates the database 'hbtn_0d_2' and the user 'user_0d_2'
+--creates DB 'hbtn_0d_2 user 'user_0d_2', sets pwd, and grants select prvg
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 SET PASSWORD FOR 'user_0d_2'@'localhost' = 'user_0d_2_pwd';
