82185de23fc6bd78a00c80d6c04ba61a14f3009a
Hasna <hasna.elboukhari.neuro@gmail.com>
2024-03-29 19:01:12 -0700

    file created

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send POST HTTP request with values for `email` and `subject` variables
-curl -sX POST -d 'email=hr@school.com' -d 'subject=I will always be here for PLD' "$1"
+curl -sX POST -d 'email=test@gmail.com' -d 'subject=I will always be here for PLD' "$1"
